1cf125d474a39d442d9b56b792701d7253552869
Mark Stosberg <mark@stosberg.com>
2012-08-31 23:35:08 -0400

    RT#66558: BugFix: We now correctly dispatch to the start_mode() when
        auto_rest is enabled and /:rm? is used and no run mode is found.

--- lib/CGI/Application/Dispatch/PSGI.pm
+++ lib/CGI/Application/Dispatch/PSGI.pm
@@ -247,7 +247,7 @@ sub as_psgi {
 
             my $auto_rest =
             defined $named_args->{auto_rest} ? $named_args->{auto_rest} : $args{auto_rest};
-            if($auto_rest) {
+            if($auto_rest && defined $rm && length $rm) {
                 my $method_lc =
                 defined $named_args->{auto_rest_lc}
                 ? $named_args->{auto_rest_lc}
