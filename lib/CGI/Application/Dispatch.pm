b13b61fda6a6208cbe60103d276f97715d83f8e1
Michael Peters <mpeters@plusthree.com>
2012-12-19 16:09:02 -0500

    http_error() needs to return some content if there is an error or other things dont work right in this case it was just under MOD_PERL that it wasn't returning anything

--- lib/CGI/Application/Dispatch.pm
+++ lib/CGI/Application/Dispatch.pm
@@ -490,6 +490,60 @@ sub http_error {
 
     }
 
+    # stolen from http_protocol.c in Apache sources
+    # we don't actually use anything other than 200, 307, 400, 404 and 500
+    my %status_lines = (
+        #    100 => 'Continue',
+        #    101 => 'Switching Protocols',
+        #    102 => 'Processing',
+        200 => 'OK',
+        #    201 => 'Created',
+        #    202 => 'Accepted',
+        #    203 => 'Non-Authoritative Information',
+        #    204 => 'No Content',
+        #    205 => 'Reset Content',
+        #    206 => 'Partial Content',
+        #    207 => 'Multi-Status',
+        #    300 => 'Multiple Choices',
+        #    301 => 'Moved Permanently',
+        #    302 => 'Found',
+        #    303 => 'See Other',
+        #    304 => 'Not Modified',
+        #    305 => 'Use Proxy',
+        307 => 'Temporary Redirect',
+        400 => 'Bad Request',
+        #    401 => 'Authorization Required',
+        #    402 => 'Payment Required',
+        #    403 => 'Forbidden',
+        404 => 'Not Found',
+        #    405 => 'Method Not Allowed',
+        #    406 => 'Not Acceptable',
+        #    407 => 'Proxy Authentication Required',
+        #    408 => 'Request Time-out',
+        #    409 => 'Conflict',
+        #    410 => 'Gone',
+        #    411 => 'Length Required',
+        #    412 => 'Precondition Failed',
+        #    413 => 'Request Entity Too Large',
+        #    414 => 'Request-URI Too Large',
+        #    415 => 'Unsupported Media Type',
+        #    416 => 'Requested Range Not Satisfiable',
+        #    417 => 'Expectation Failed',
+        #    422 => 'Unprocessable Entity',
+        #    423 => 'Locked',
+        #    424 => 'Failed Dependency',
+        500 => 'Internal Server Error',
+        #    501 => 'Method Not Implemented',
+        #    502 => 'Bad Gateway',
+        #    503 => 'Service Temporarily Unavailable',
+        #    504 => 'Gateway Time-out',
+        #    505 => 'HTTP Version Not Supported',
+        #    506 => 'Variant Also Negotiates',
+        #    507 => 'Insufficient Storage',
+        #    510 => 'Not Extended',
+    );
+
+
     # if we're under mod_perl
     if(IS_MODPERL) {
         my $r = $self->_r;
@@ -497,67 +551,8 @@ sub http_error {
 
         # if we just want to redirect
         $r->headers_out->{'Location'} = $url if $url;
-        return '';
+        return $status_lines{$errno} || "HTTP Error: $errno";
     } else {    # else print the HTTP stuff ourselves
-
-        # stolen from http_protocol.c in Apache sources
-        # we don't actually use anything other than 200, 307, 400, 404 and 500
-
-        my %status_lines = (
-
-            #    100 => 'Continue',
-            #    101 => 'Switching Protocols',
-            #    102 => 'Processing',
-            200 => 'OK',
-
-            #    201 => 'Created',
-            #    202 => 'Accepted',
-            #    203 => 'Non-Authoritative Information',
-            #    204 => 'No Content',
-            #    205 => 'Reset Content',
-            #    206 => 'Partial Content',
-            #    207 => 'Multi-Status',
-            #    300 => 'Multiple Choices',
-            #    301 => 'Moved Permanently',
-            #    302 => 'Found',
-            #    303 => 'See Other',
-            #    304 => 'Not Modified',
-            #    305 => 'Use Proxy',
-            307 => 'Temporary Redirect',
-            400 => 'Bad Request',
-
-            #    401 => 'Authorization Required',
-            #    402 => 'Payment Required',
-            #    403 => 'Forbidden',
-            404 => 'Not Found',
-
-            #    405 => 'Method Not Allowed',
-            #    406 => 'Not Acceptable',
-            #    407 => 'Proxy Authentication Required',
-            #    408 => 'Request Time-out',
-            #    409 => 'Conflict',
-            #    410 => 'Gone',
-            #    411 => 'Length Required',
-            #    412 => 'Precondition Failed',
-            #    413 => 'Request Entity Too Large',
-            #    414 => 'Request-URI Too Large',
-            #    415 => 'Unsupported Media Type',
-            #    416 => 'Requested Range Not Satisfiable',
-            #    417 => 'Expectation Failed',
-            #    422 => 'Unprocessable Entity',
-            #    423 => 'Locked',
-            #    424 => 'Failed Dependency',
-            500 => 'Internal Server Error',
-
-            #    501 => 'Method Not Implemented',
-            #    502 => 'Bad Gateway',
-            #    503 => 'Service Temporarily Unavailable',
-            #    504 => 'Gateway Time-out',
-            #    505 => 'HTTP Version Not Supported',
-            #    506 => 'Variant Also Negotiates',
-            #    507 => 'Insufficient Storage',
-            #    510 => 'Not Extended',
-        );
 
         $errno = 500 if(!exists $status_lines{$errno});
 
